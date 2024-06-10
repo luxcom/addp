184e9a7a3e74fa5a2a0dd3d1d65ffec55c82f79b
Neil Connolly <n4151027@gmail.com>
2024-06-10 18:31:40 -0400

    Converted to python3, discover working.

--- device.py
+++ device.py
@@ -13,37 +13,37 @@ import socket
 from addp import parse_frame, build_response
 
 if __name__ == "__main__":
-	HOST, PORT = "224.0.5.128", 2362
+        HOST, PORT = "224.0.5.128", 2362
 
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	try:
-		sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-	except AttributeError:
-		pass
-	mreq = struct.pack("4sI", socket.inet_aton(HOST),
-					   socket.INADDR_ANY)
-	sock.setsockopt(socket.IPPROTO_IP,
-						   socket.IP_ADD_MEMBERSHIP, mreq)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        try:
+                sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        except AttributeError:
+                pass
+        mreq = struct.pack("4sI", socket.inet_aton(HOST),
+                                           socket.INADDR_ANY)
+        sock.setsockopt(socket.IPPROTO_IP,
+                                                   socket.IP_ADD_MEMBERSHIP, mreq)
 
-	sock.bind(("", PORT))
+        sock.bind(("", PORT))
 
-	sys.stderr.write('Listening for ADDP requests on port {0}, Ctrl-C to stop.\n'.format(PORT))
-	while True:
-		try:
-			data, addr = sock.recvfrom(2048)
-		except KeyboardInterrupt:
-			print 'Exiting...'
-			sys.exit(0)
+        sys.stderr.write('Listening for ADDP requests on port {0}, Ctrl-C to stop.\n'.format(PORT))
+        while True:
+                try:
+                        data, addr = sock.recvfrom(2048)
+                except KeyboardInterrupt:
+                        print('Exiting...')
+                        sys.exit(0)
 
-		if data is None or data == '':
-			break
+                if data is None or data == '':
+                        break
 
-		info = parse_frame(data)
+                info = parse_frame(data)
 
-		if info:
-			print 'Responing to discover from', addr
-			ret = build_response(info)
-			if ret and ret != "":
+                if info:
+                        print('Responing to discover from', addr)
+                        ret = build_response(info)
+                	if ret and ret != "":
 				sock.sendto(ret, addr)
 
--- discover.py
+++ discover.py
@@ -6,47 +6,50 @@ Discover ADDP devices on the local network
 
 import sys
 import socket
+import struct
 from pprint import pprint
 
 from addp import build_request, parse_frame
 
 def send_discovery():
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
 
-	# you can only use the REUSE options OR the bind, not both
-	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	try:
-		sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-	except AttributeError:
-		pass
-	
-	sock.bind(("", 2362))
+        # you can only use the REUSE options OR the bind, not both
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        try:
+                sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        except AttributeError:
+                pass
+        
+        sock.bind(("", 2362))
+        mreq = struct.pack("4sl", socket.inet_aton('224.0.5.128'), socket.INADDR_ANY)
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+        sock.settimeout(1.0)
+        msg = build_request(0x01, mac=(255,255,255,255,255,255))
 
-	sock.settimeout(1.0)
-	msg = build_request(0x01, mac=(255,255,255,255,255,255))
+        sock.sendto(msg, ("224.0.5.128", 2362))
 
-	sock.sendto(msg, ("224.0.5.128", 2362))
+        responses = []
+        while True:
+                try:
+                        data, addr = sock.recvfrom(2048)
+                        print(data.hex())
+                except:
+                        break
 
-	responses = []
-	while True:
-		try:
-			data, addr = sock.recvfrom(2048)
-		except:
-			break
+                if data is None or data == '':
+                        break
 
-		if data is None or data == '':
-			break
+                info = parse_frame(data)
+                if info:
+                        info['addp_ip'] = addr[0]
+                        responses.append(info)
 
-		info = parse_frame(data)
-		if info:
-			info['addp_ip'] = addr[0]
-			responses.append(info)
-
-	sock.close()
-	return responses
+        sock.close()
+        return responses
 
 if __name__ == '__main__':
-	sys.stderr.write('Sending discover message...\n')
-	pprint(send_discovery())
+        sys.stderr.write('Sending discover message...\n')
+        pprint(send_discovery())
 
